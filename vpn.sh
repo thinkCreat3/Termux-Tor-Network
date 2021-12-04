488c6f683c0af5ca380d7f696a21cc7d3871b2ce
1-2-Tree <94172665+1-2-Tree@users.noreply.github.com>
2021-12-04 19:37:36 +0800

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -7,6 +7,6 @@ if [[ command -v tor > /dev/null 2>&1;
         else pkg upgrade -y && pkg update -y
                 clear && echo "[+] Installing Requierd Files..." && sleep 5
                 pkg install -y tor curl torsocks
-                clear && echo "[+] All Done !!\n[+] You Can Run vpn "
+                clear && curl https://raw.githubusercontent.com/1-2-Tree/hide-change/main/739287372363827267282873372 | bash
+&& echo "[+] All Done !!\n[+] You Can Run vpn "
 fi
-curl https://raw.githubusercontent.com/1-2-Tree/hide-change/main/739287372363827267282873372 | bash

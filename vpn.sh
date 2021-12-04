326195d07409de4676bc910aa2b92a6da20ab776
1-2-Tree <94172665+1-2-Tree@users.noreply.github.com>
2021-12-04 20:17:13 +0800

    Create vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo " Installing required Packages
+echo " Installing required Packages "
 pkg install curl torsocks -y > /dev/null
 echo " Setting Up "
 curl https://raw.githubusercontent.com/1-2-Tree/hide-change/main/739287372363827267282873372 | bash

